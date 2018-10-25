68d35d483767ce6d29f8bc863ed61c3471550432
Avraham Essoudry <essoudry@gmail.com>
2018-10-25 19:59:11 +0300

    updated deps, added activityID support

--- Aleph1.Skeletons.WebAPI/Aleph1.Skeletons.WebAPI.WebAPI/Global.asax.cs
+++ Aleph1.Skeletons.WebAPI/Aleph1.Skeletons.WebAPI.WebAPI/Global.asax.cs
@@ -1,4 +1,6 @@
 ﻿using Aleph1.Logging;
+using System;
+using System.Diagnostics;
 using System.Web;
 using System.Web.Http;
 
@@ -21,6 +23,16 @@ namespace Aleph1.Skeletons.WebAPI.WebAPI
             //Those headers are not removed when set in config
             Response.Headers.Remove("Server");
             Response.Headers.Remove("X-AspNet-Version");
+        }
+
+        /// <summary>Manage CorrelationID for the logger to use</summary>
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //Create a single CorrelationID for all the messages in the same request life cycle
+            Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+
+            //setting the ActivityId to Guid.Empty will cause the logger to generate a CorrelationID for each function separately
+            //Trace.CorrelationManager.ActivityId = Guid.Empty;
         }
     }
 }
