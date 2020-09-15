78dcc999628e232b6dee56121cdf79c0ff5b422b
Avraham Essoudry <essoudry@gmail.com>
2020-09-15 22:56:48 +0300

    handling errors

--- Aleph1.Skeletons.WebAPI/Aleph1.Skeletons.WebAPI.WebAPI/Pages/src/shells/login.ts
+++ Aleph1.Skeletons.WebAPI/Aleph1.Skeletons.WebAPI.WebAPI/Pages/src/shells/login.ts
@@ -1,10 +1,17 @@
 import { UserService } from "resources/services";
 import { autoinject } from "aurelia-framework";
 import { LoginModel } from "resources/models";
+import { handleErrors } from "resources/decorators/handle-errors";
 
 @autoinject
 export class LoginSell
 {
 	credentials: LoginModel = new LoginModel();
 	constructor(public userService: UserService) { }
+
+	@handleErrors("התרחשה שגיאה בעת ההתחברות")
+	public login(): Promise<void>
+	{
+		return this.userService.login(this.credentials);
+	}
 }
